950efed63d550280bed07bd0ffe6d4e10352b36d
MarVasilieva <marina_vasilieva@list.ru>
2022-12-16 22:27:35 +0600

    add event

---
+++ script.js
@@ -0,0 +1,12 @@
+function changeThems (){
+    switch (document.getElementById('thems').value){
+        case "white" : document.getElementById('body').style.backgroundColor = "white"; break;
+        case "blue" : document.getElementById('body').style.backgroundColor = "blue"; break;
+        case "yellow" : document.getElementById('body').style.backgroundColor = "yellow"; break;
+        case "red" : document.getElementById('body').style.backgroundColor = "red"; break;
+        case "green" : document.getElementById('body').style.backgroundColor = "green"; break;
+        
+
+    }
+   
+}
